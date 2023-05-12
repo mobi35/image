c27d464e9001609c10ea180e657df6a0d74901cf
adrian <mobistyle35@gmail.com>
2023-05-13 03:34:12 +0800

    fx

---
+++ tests/TestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Adi\Image\Tests;
+
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+  public function setUp(): void
+  {
+    parent::setUp();
+    // additional setup
+  }
+
+  protected function getPackageProviders($app)
+  {
+    return [
+
+    ];
+  }
+
+  protected function getEnvironmentSetUp($app)
+  {
+    // perform environment setup
+  }
+}
---
+++ tests/Unit/AdiTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Adi\Image\Tests\Unit;
+
+use Adi\Image\Tests\TestCase;
+
+class AdiTest extends TestCase {
+
+  public function test_it_works()
+  {
+    $this->assertEquals(1,1);
+  }
+}
